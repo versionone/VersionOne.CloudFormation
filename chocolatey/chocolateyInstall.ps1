b7fc6cdb8f8f99b3b576fb7bd9c595e18101cdab
Josh Gough <jsgough@gmail.com>
2013-10-02 17:56:43 -0400

    Modified to use Get-ChocolateyWebFile and Get-ChocolateyUnzip per discussion with Joe
    The -exclude is not actually working here. I'm not sure if it can actually be combined with -recurse :/

--- chocolatey/chocolateyInstall.ps1
+++ chocolatey/chocolateyInstall.ps1
@@ -1,10 +1,19 @@
 try {
-  $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-  Install-ChocolateyZipPackage 'VersionOne' 'http://www.versionone.com/download$/z2n11665/V1.13.b795g21v.zip' $toolsDir
+  $chocTempDir = Join-Path $env:TEMP "chocolatey"
+  $tempDir = Join-Path $chocTempDir "$packageName"
+  $zipFile = 'VersionOne.zip'
+  $file = Join-Path $tempDir $zipFile
+  $logFile = Join-Path $tempDir "VersionOneSetup.log"
+  $url = 'http://www.versionone.com/download$/z2n11665/V1.13.b795g21v.zip' 
+  $url64 = $url
+  Get-ChocolateyWebFile $packageName $file $url $url64
+  Get-ChocolateyUnzip $file $tempDir
+  
+  $silentArgs = '-Quiet:2 -SigningKey:1BBD93C864A6001737BAA148DCDDB67E927982FA6A59E7DF53CDBDF0DD3CE29A -DbServer:(local) -LogFile:"' + $logFile + '" -DBName:V1CHOC V1CHOC'
 
-  $v1FileFullPath = get-childitem $toolsDir -recurse -include VersionOne.Setup*.exe | select -First 1
-  Install-ChocolateyInstallPackage 'VersionOne' 'exe' '-Quiet:2 -SigningKey:1BBD93C864A6001737BAA148DCDDB67E927982FA6A59E7DF53CDBDF0DD3CE29A -DbServer:(local) -LogFile:C:\Projects\v1Package\setup.log -DBName:V1CHOC V1CHOC' "$v1FileFullPath"
-  Remove-Item "$v1FileFullPath"
+  $v1FileFullPath = get-childitem $tempDir -recurse -include VersionOne.Setup*.exe | select -First 1
+  Install-ChocolateyInstallPackage 'VersionOne' 'exe' $silentArgs "$v1FileFullPath"
+  Remove-Item $tempDir -recurse -exclude "$logFile"
   Write-ChocolateySuccess 'VersionOne'
 } catch {
   Write-ChocolateyFailure 'VersionOne' "$($_.Exception.Message)"
