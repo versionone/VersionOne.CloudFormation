aeb657e24ed3ee96c5af672be037325d16b682b0
Josh Gough <jsgough@gmail.com>
2013-10-17 16:55:06 -0400

    Added support for setting environment variables in the form of: set V1<installer param name>=value to override instsallation defaults.
    Example:
    
    ```
    set V1DBName=V1Db
    set V1InstanceName=V1Agile
    set V1DBServer=v1dbserver
    cinst VersionOne -source '%CD%'
    ```
    
    That will change from the defaults of:
    VersionOne
    VersionOne
    (local)

--- chocolatey/chocolateyInstall.ps1
+++ chocolatey/chocolateyInstall.ps1
@@ -6,11 +6,36 @@ try {
   $logFile = Join-Path $tempDir "VersionOneSetup.log"
   $url = 'http://www.versionone.com/download$/z2n11665/V1.13.b795g21v.zip' 
   $url64 = $url
+
+  if ($env:V1URL) {
+    $url = $env:V1URL
+    $url64 = $url
+  }
+
+  $v1Params = @{
+    "Quiet" = "2";
+    "DbServer" = "(local)";
+    "DbName" = "VersionOne";
+    "InstanceName" = "VersionOne";
+    "LogFile" = Join-Path $tempDir "VersionOneSetup.log";
+  }
+
+  if (!$silentArgs) {  
+    Get-ChildItem Env: | Where { $_.Key.ToUpper().StartsWith("V1") } | foreach { 
+      $paramName = $_.Key.Substring(2)
+      $v1Params[$paramName] = $_.Value
+    }
+    $silentArgs = ""
+    foreach ($key in $v1Params.Keys) {
+      if ($key -ne "InstanceName") {
+        $silentArgs = $silentArgs + " -" + $key + ":" + $v1Params[$key]
+      }
+    }
+    $silentArgs = $silentArgs + " " + $v1Params["InstanceName"]
+  }
   Get-ChocolateyWebFile $packageName $file $url $url64
   Get-ChocolateyUnzip $file $tempDir
   
-  $silentArgs = '-Quiet:2 -SigningKey:1BBD93C864A6001737BAA148DCDDB67E927982FA6A59E7DF53CDBDF0DD3CE29A -DbServer:(local) -LogFile:"' + $logFile + '" -DBName:V1CHOC V1CHOC'
-
   $v1FileFullPath = get-childitem $tempDir -recurse -include VersionOne.Setup*.exe | select -First 1
   Install-ChocolateyInstallPackage 'VersionOne' 'exe' $silentArgs "$v1FileFullPath"
   Remove-Item $tempDir -recurse -exclude "$logFile"
